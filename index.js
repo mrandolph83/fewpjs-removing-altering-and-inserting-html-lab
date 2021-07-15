52a71432bb74a86ec3eb919bbd0ab3e84fa9e48a
mrandolph83 <mrandolph83@gmail.com>
2021-07-15 00:05:58 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let maingone = document.getElementById('main')
+maingone.remove();
+
+
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.id = 'victory'
+newHeader.innerHTML = "Matt is the champion";
